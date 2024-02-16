1ca4bf2e4df9284181236d2b9af8bc7cedcd1fea
NyamburaKariuki <prettimercii@gmail.com>
2024-02-16 14:47:28 +0300

    bubblesort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from small to large value
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t x, index, temp = 0;
+
+	if (size < 2)
+		return;
+	for (x = 0; x < size; index++)
+	{
+		if (array[index] > array[index + 1] && array[index + 1])
+		{
+			temp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = temp;
+			print_array(array, size);
+		}
+	}
+}
